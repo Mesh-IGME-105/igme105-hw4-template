803367142e261f0109e46e71f105e229ff5810e9
Erika Mesh <esmvcs@rit.edu>
2025-11-04 20:27:32 -0500

    Uncommented the Enemy starter constructor

--- HW4_TheArena/Enemy.cs
+++ HW4_TheArena/Enemy.cs
@@ -8,11 +8,9 @@ namespace HW4_TheArena
         // TODO: Add appropriate constants and fields as needed
 
         // TODO: Complete the parameterized constructor that takes in the enemy's name and an instance of Random
-        /*
         public Enemy(string name, Random rng)
         {
         }
-        */
 
         // TODO: Create a method to apply damage to the enemy and return the actual damage taken
         // The actual damage applied should be a random value based on the paramter value
